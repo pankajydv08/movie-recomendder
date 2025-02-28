1527bf12893a40bedf94dbed586ec7af4baa853c
Pankaj Yadav <157686378+pankajydv07@users.noreply.github.com>
2025-02-28 15:20:57 +0530

    Update omdb.ts

--- src/lib/omdb.ts
+++ src/lib/omdb.ts
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-const API_KEY = import.meta.env.VITE_OMDB; // Use environment variable for OMDB API key
+// const API_KEY = import.meta.env.VITE_OMDB; // Use environment variable for OMDB API key
 
-if (!API_KEY) {
-  throw new Error('OMDB API key is missing. Please set it in the environment variables.');
-}else{
-  console.log("API working")
-}
+// if (!API_KEY) {
+//   throw new Error('OMDB API key is missing. Please set it in the environment variables.');
+// }else{
+//   console.log("API working")
+// }
 
 export async function getMoviePoster(title: string): Promise<string | null> {
   try {
-    const response = await axios.get(`https://www.omdbapi.com/?t=${encodeURIComponent(title)}&apikey=${API_KEY}`);
+    const response = await axios.get(`https://www.omdbapi.com/?t=${encodeURIComponent(title)}&apikey=16cd21bc`);
     if (response.data && response.data.Poster) {
       return response.data.Poster;
     } else {
